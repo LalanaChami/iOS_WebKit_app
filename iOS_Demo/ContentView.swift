c22610da115718296aea16f72a56c0bfe5868a6b
LalanaChami <lalanachamika123@gmail.com>
2020-09-03 15:28:37 +0530

    Initial Commit

---
+++ iOS_Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_Demo
+//
+//  Created by Web Technologies on 9/3/20.
+//  Copyright © 2020 Web Technologies. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
