5eda576e809fafe472ca14968fcc04a9056f2565
LalanaChami <lalanachamika123@gmail.com>
2020-09-03 23:09:18 +0530

    Using web kit swift

--- iOS_Demo/ContentView.swift
+++ iOS_Demo/ContentView.swift
@@ -2,17 +2,53 @@
 //  ContentView.swift
 //  iOS_Demo
 //
-//  Created by Web Technologies on 9/3/20.
-//  Copyright © 2020 Web Technologies. All rights reserved.
+//  Created by Lalana on 9/3/20.
+//  Copyright © 2020 Lalana. All rights reserved.
 //
 
 import SwiftUI
+import WebKit
+
+//navigation view
 
 struct ContentView: View {
+    
+    @State private var selectedSegment = 1
+    @State private var websites = ["developer.apple.com","web.dev/"]
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            VStack{
+                HStack(alignment: .lastTextBaseline){
+                    Picker("",selection: $selectedSegment){
+                        ForEach(0 ..< websites.count){
+                            Text(self.websites[$0]).tag($0)
+                        }
+                    }.frame(width: 300,height: 160,alignment: .top)
+                }
+                WebView(request: URLRequest(url: URL(string: "https://\(websites[selectedSegment])")!)).frame(width: 420, height: 700, alignment: .bottom)
+            }
+        }
     }
 }
+
+
+struct WebView :UIViewRepresentable {
+    
+    let request : URLRequest
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(request)
+    }
+    
+}
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
